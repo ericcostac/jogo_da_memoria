c3f320fa7a197b3c18a485599cd426077617b251
Eric Alberto Costa e Costa <71726316+ericcostac@users.noreply.github.com>
2020-10-22 15:42:33 -0300

    implementando mecanismos js para lógica do game

--- game_da_memoria/script.js
+++ game_da_memoria/script.js
@@ -1,9 +1,35 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard(){
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    //primeiro clique no card
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    //certificação de que a primeira e segunda carta são iguais/identicas
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath ();
 }
+
+function checkForMath() {
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+
 
 cards.forEach((card) =>{
     card.addEventListener('click', flipCard)
 })
+
+ 
